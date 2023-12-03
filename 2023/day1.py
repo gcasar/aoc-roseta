d2de549aedf7e35c4348c0cd0f57bdb0bb885fab
Gregor Casar <gregorcasar@gmail.com>
2023-12-03 08:28:17 +0000

    Day 1 star2

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,47 +1,26 @@
 import sys
 import re
 
-# Matches either strings with first and last digit
-twonums = re.compile(r'\D*(\d).*(\d)\D*')
-# ... with only a single digit in string
-onenum = re.compile(r'\D*(\d)\D*')
-# Sets up a regex to only replace a single digit or the first and last one
 number_strings = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-numbers_group = '|'.join(number_strings)
-twostring = re.compile(fr"({numbers_group}).*({numbers_group})")
-onestring = re.compile(fr"({numbers_group})")
+replacements_dict = { 
+    **{ str(i): i for i in range(1, 10)}, 
+    ** { number_strings[i]: i+1 for i in range(9) }
+}
+okmatch = re.compile(fr"(\d|one|two|three|four|five|six|seven|eight|nine)")
 
-def replace_span( input_string, span ):
-    start, end = span
-    string_to_replace = input_string[start:end]
-    replacement = str(number_strings.index(string_to_replace)+1)
-    return input_string[:start] + replacement + input_string[end:]
+def get_digits( line ):
+    """Removes invalid chars one by one, left to right"""
+    match = okmatch.search(line)
+    while match:
+        yield replacements_dict[match.group(1)]
+        line = line[match.start()+1:]
+        match = okmatch.search(line)
 
-def preprocess_line( line ):
-    """Replaces the first and numeric string with its digit representations"""
-    # Replace the last number if it exists
-    match = twostring.search(line)
-    if match:
-        line = replace_span(line, match.span(2))
-
-    # Now the first one
-    match = onestring.search(line)
-    if match:
-        line = replace_span(line, match.span(1))
-    
-    return line
-
-
-buffer = list(map(lambda x: x, sys.stdin))
-print(*list(map(preprocess_line, buffer)), sep='')
 
 def process_input( stream ):
     for line in stream:
-        line = preprocess_line(line)
-        if twonums.match(line):
-            yield int(twonums.sub(r'\1\2', line))
-        elif onenum.match(line):
-            yield int(onenum.sub(r'\1\1', line))
+        digits = list(get_digits(line))
+        yield int(digits[0] * 10 + digits[-1])
 
 
-print(sum(process_input(buffer)))
+print(sum(process_input(sys.stdin)))
